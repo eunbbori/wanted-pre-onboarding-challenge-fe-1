5d176159e79771f041ec4b36c0ebec29e8b7e621
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2023-01-01 22:13:47 +0900

    2023-01-01 글로벌 색깔 테마 설정

--- client/src/styles/Theme.ts
+++ client/src/styles/Theme.ts
@@ -1,1 +1,12 @@
-export {};
+import { DefaultTheme } from "styled-components";
+
+const Theme: DefaultTheme = {
+  color: {
+    color1: "#439A97",
+    color2: "#62B6B7",
+    color3: "#97DECE",
+    color4: "#CBEDD5",
+  },
+};
+
+export default Theme;
--- client/src/styles/style.d.ts
+++ client/src/styles/style.d.ts
@@ -1,1 +1,12 @@
-export {};
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    color: {
+      color1: string;
+      color2: string;
+      color3: string;
+      color4: string;
+    };
+  }
+}
