fedd3f7e33bb986e2eb2c5c3df35a40ecc6e6e92
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2023-01-08 15:47:47 +0900

    2023-01-08 readOnly 모드 state 관리 및 input,textarea useState -> useRef 로 변경

--- client/src/pages/Todo/MainTodo/components/DetailTodo.tsx
+++ client/src/pages/Todo/MainTodo/components/DetailTodo.tsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import DB_DOMAIN_URL from "../../../../utils/DB_DOMAIN_URL";
 import axios from "axios";
 import { BsFillPencilFill } from "react-icons/bs";
@@ -13,33 +13,52 @@ import {
 } from "./DetailTodoStyle";
 
 const DetailTodo = () => {
-  const [task, setTask] = useState({ title: "title", content: "content" });
+  const titleRef = useRef<HTMLInputElement>(null);
+  const contentRef = useRef<HTMLTextAreaElement>(null);
   const [editMode, setEditMode] = useState(false);
+  const [readOnly, setReadOnly] = useState(true);
   const { id } = useParams();
   const token = localStorage.getItem("login-token");
   const editHandler = () => {
     setEditMode(true);
+    setReadOnly(false);
   };
+  const cancelAndSaveHandler = () => {
+    setEditMode(false);
+    setReadOnly(true);
+  };
+
   useEffect(() => {
     axios
       .get(`${DB_DOMAIN_URL}/todos/${id}`, {
         headers: { Authorization: token },
       })
       .then((response) => {
-        setTask(response.data.data);
+        if (titleRef.current && contentRef.current) {
+          titleRef.current.value = response.data.data.title;
+          contentRef.current.value = response.data.data.content;
+        }
       });
   }, [id, token]);
 
   return (
     <Container>
       <TitleContainer>
-        <input type="text" value={task.title} readOnly />
+        <input
+          type="text"
+          name="title"
+          defaultValue="title"
+          readOnly={readOnly}
+          ref={titleRef}
+        />
       </TitleContainer>
       <ContentContainer>
         <textarea
           style={{ width: "100%", height: "100%" }}
-          readOnly
-          value={task.content}
+          readOnly={readOnly}
+          defaultValue="content"
+          name="content"
+          ref={contentRef}
         />
       </ContentContainer>
       {!editMode && (
@@ -57,6 +76,7 @@ const DetailTodo = () => {
           className={"editBtn"}
           type={"button"}
           text={"CANCEL"}
+          onClick={cancelAndSaveHandler}
         />
       )}
       {editMode && (
@@ -65,6 +85,7 @@ const DetailTodo = () => {
           className={"editBtn"}
           type={"button"}
           text={"SAVE"}
+          onClick={cancelAndSaveHandler}
         />
       )}
     </Container>
