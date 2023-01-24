24898d7a012ed516d75d178d864f1809bd83e070
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2023-01-25 00:04:48 +0900

    기존 todoDetail 수정 react-query 로직 변경

---
+++ client/src/hook/todo/useUpdateTodo.ts
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import authAxios from "../../queries/axios";
+import { TODO } from "../../utils/queryKeys";
+import { TodoInfo } from "../../type/todoInfo";
+
+const updateTodo = async (
+  token: string,
+  updatedTodo: TodoInfo,
+): Promise<TodoInfo> => {
+  return authAxios
+    .put(`/todos/${updatedTodo.id}`, updatedTodo, {
+      headers: {
+        Authorization: token,
+      },
+    })
+    .then((data) => data.data.data);
+};
+
+export default function useUpdateTodo(token: string) {
+  const queryClient = useQueryClient();
+  return useMutation(
+    (updatedTodo: TodoInfo) => updateTodo(token, updatedTodo),
+    {
+      onSuccess: () => queryClient.invalidateQueries([TODO]),
+    },
+  );
+}
